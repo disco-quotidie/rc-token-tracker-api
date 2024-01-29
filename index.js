ac375027ae6b8b47b5be3cc7f3124b26b4554d1a
disco-quotidie <ccprasong@outlook.com>
2024-01-30 05:10:46 +0700

    token scan update

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ const http = require('http');
 
 const server = http.createServer(app);
 
+global.tokens = ['ncat', 'cado']
+global.holderMap = {}
+
 server.listen(3000, () => {
     console.log('SERVER');
     console.log(`Listening to port 3000`);
 
-    // scanHolders()
+    global.tokens.map(tick => scanHolders(tick))
 });
 
-global.holderMap = {}
-
 setInterval(() => {
-    scanHolders();
+    global.tokens.map(tick => scanHolders(tick))
 }, 1000*60*10)
--- scanHolders.js
+++ scanHolders.js
@@ -1,6 +1,9 @@
 const axios = require('axios')
-const scanHolders = async (tick = 'fren') => {
+const scanHolders = async (tick = 'ncat') => {
 
+    if(global.tokens.indexOf(tick) <= -1)
+        global.tokens.push(tick)
+    
     let no_more = false
     let addr_list = []
     let balance_list = []
@@ -9,7 +12,7 @@ const scanHolders = async (tick = 'fren') => {
     let i = 0
     while (!no_more) {
 
-        console.log(`calling: https://api-prod.cybord.org/predicates/${tick}?offset=${i * 100}&limit=100`)
+        // console.log(`calling: https://api-prod.cybord.org/predicates/${tick}?offset=${i * 100}&limit=100`)
         const response = await axios.get(`https://api-prod.cybord.org/predicates/${tick}?offset=${i * 100}&limit=100`)
 
         if (response.data.length === 0) {
@@ -43,24 +46,24 @@ const scanHolders = async (tick = 'fren') => {
     for (let i = 0; i < addr_list.length; i ++) {
         const address = addr_list[i]
 
-        console.log(`calling https://api-prod.cybord.org/balance/${address}`)
+        // console.log(`calling https://api-prod.cybord.org/balance/${address}`)
         const response = await axios.get(`https://api-prod.cybord.org/balance/${address}`)
 
         const { data } = response
-        console.log(data)
+        // console.log(data)
 
         for(let j = 0; j < data.length; j ++) {
             const rec = data[j]
             if(rec.tick === tick) {
                 if (rec.amt && parseInt(rec.amt) > 0)
-                    balance_list.push ( {address, amount: rec.amt})
+                    balance_list.push ( {_address: address, _amount: rec.amt})
             }
         }
     }
 
     console.log(`-------------------------Scan finished for ${tick} at ${getDateStr()}}------------------------------\n`)
 
-    balance_list.sort((n1, n2) => n2.amount - n1.amount)
+    balance_list.sort((n1, n2) => n2._amount - n1._amount)
 
     global.holderMap[tick] = balance_list;
 }
